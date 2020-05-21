8c646693e03288927842481583d121e9e7659b0b
ThomasFortrie <thomas.fortrie@hotmail.fr>
2020-05-21 18:57:53 +0200

    ajout img card

--- functions.php
+++ functions.php
@@ -4,6 +4,7 @@
 function supportDuTheme(){
 
     add_theme_support('title-tag');
+    add_theme_support( 'post-thumbnails' );
 }
 
 function theme_register_asset(){
--- index.php
+++ index.php
@@ -4,28 +4,30 @@
 <?php if (have_posts()) : ?>
 
     <div class="row justify-content-around">
-    <?php while (have_posts()) : the_post(); ?>
-            <div class="card" style="width: 18rem;">
-                <!-- <img src="..." class="card-img-top" alt="..."> -->
-                <div class="card-body">
-                    <h5 class="card-title"><?php the_title() ?> - par <?php the_author() ?></h5>
-                    <p class="card-text">
-                        <?php the_post_thumbnail();
-                        the_excerpt() ?>
-                    </p>
-                    <a href="<?php the_permalink() ?>" class="btn btn-primary">Go somewhere</a>
+        <?php while (have_posts()) : the_post(); ?>
+            <div class="col-sm-4">
+                <div class="card" style="width: 18rem;">
+                   <?php the_post_thumbnail('medium', ['class' => 'card-img-top', 'atl' => '', 'style' => 'heigth: auto'] ) ?>
+                    <div class="card-body">
+                        <h5 class="card-title"><?php the_title() ?> - par <?php the_author() ?></h5>
+                        <br><? the_category() ?>
+                        <p class="card-text">
+                            <?php the_excerpt() ?>
+                        </p>
+                        <a href="<?php the_permalink() ?>" class="card-link">Voir plus</a>
+                    </div>
                 </div>
             </div>
 
 
 
         <?php endwhile ?>
-        </div>
-    <?php else : ?>
-        <h1>Pas d'articles</h1>
-    <?php endif; ?>
+    </div>
+<?php else : ?>
+    <h1>Pas d'articles</h1>
+<?php endif; ?>
 
 
 
 
-    <?php get_footer() ?>
+<?php get_footer() ?>
